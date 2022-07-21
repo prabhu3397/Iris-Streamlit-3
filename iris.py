71aaf997a7737267606f6a37fb83eb5bb1dc368d
prabhu3397 <74896218+prabhu3397@users.noreply.github.com>
2022-07-21 15:01:20 +0800

    Update iris.py

--- iris.py
+++ iris.py
@@ -1,7 +1,11 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
+
 from sklearn.ensemble import RandomForestClassifier
+
+df=pd.read_csv('IRIS')
+
+
 
 st.write("""
 # Simple Iris Flower Prediction App
